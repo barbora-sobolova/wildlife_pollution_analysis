bdfa9138dd543b03fa25e2f7306cba8c2f2bd95c
Barbora Němcová <barbora.sobolova@kit.edu>
2025-09-03 07:53:40 +0200

    Add unit tests for the spline confidence intervals

---
+++ tests/test_spline_ci.R
@@ -0,0 +1,43 @@
+source(here::here("functions", "fit_interval_regression.R"))
+source(here::here("functions", "helper_functions.R"))
+source(here::here("functions", "plot_elements.R"))
+
+test_that("Manual calculation of the spline fit is correct", {
+  
+  # Load the data
+  df_detected_by_category <- read_csv(
+    here::here("data", "data_by_pollutant_category.csv")
+  ) |>
+    filter(Sample_number != "Z91")
+  
+  # What is the time difference between the first and the last observation?
+  timeline_length <- df_detected_by_category$Date_of_sample_collection |>
+    unify_year() |>
+    range() |> 
+    diff() |> 
+    as.numeric()
+  
+  # Fit the model
+  results <- fit_interval_reg(df_detected_by_category, plot_results = FALSE)
+  
+  # New data frame for the `predict()` function
+  newdata <- data.frame(
+    Date_numeric = seq(from = 0, to = timeline_length, by = 1),
+    Park = "Bay_Wald",
+    Age = "fawn"
+  )
+  for (k in seq_along(results$fitted_mods)) {
+    fit_auto <- predict(results$fitted_mods[[k]], newdata = newdata, se = TRUE)
+    fit_manual <- calculate_spline_ci(
+      results$fitted_mods[[k]],
+      timeline_length,
+      intercept = TRUE
+    )
+    
+    # Skip test, if fitting not possible
+    if (!anyNA(fit_auto$fit)) {
+      expect_true(all(fit_auto$fit - fit_manual$fit < 1e-14))
+      expect_true(all(fit_auto$se.fit - fit_manual$se < 1e-14)) 
+    }
+  }
+})
