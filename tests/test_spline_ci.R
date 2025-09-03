1cd88b94bbfd1d8512d13181368c035333f95348
Barbora Němcová <barbora.sobolova@kit.edu>
2025-09-03 09:08:50 +0200

    Add the test of centering

--- tests/test_spline_ci.R
+++ tests/test_spline_ci.R
@@ -5,23 +5,23 @@ source(here::here("functions", "helper_functions.R"))
 source(here::here("functions", "plot_elements.R"))
 
 test_that("Manual calculation of the spline fit is correct", {
-  
+
   # Load the data
   df_detected_by_category <- read_csv(
     here::here("data", "data_by_pollutant_category.csv")
   ) |>
     filter(Sample_number != "Z91")
-  
+
   # What is the time difference between the first and the last observation?
   timeline_length <- df_detected_by_category$Date_of_sample_collection |>
     unify_year() |>
-    range() |> 
-    diff() |> 
+    range() |>
+    diff() |>
     as.numeric()
-  
+
   # Fit the model
   results <- fit_interval_reg(df_detected_by_category, return_plots = FALSE)
-  
+
   # New data frame for the `predict()` function
   newdata <- data.frame(
     Date_numeric = seq(from = 0, to = timeline_length, by = 1),
@@ -35,11 +35,23 @@ test_that("Manual calculation of the spline fit is correct", {
       timeline_length,
       intercept = TRUE
     )
-    
-    # Skip test, if fitting not possible
+    fit_manual_centered <- calculate_spline_ci(
+      results$fitted_mods[[k]],
+      timeline_length,
+      intercept = FALSE
+    )$fit
+    fit_auto_centered <- fit_auto$fit /
+      exp(coefficients(results$fitted_mods[[k]])["(Intercept)"])
+
+    # Skip tests, if fitting not possible
     if (!anyNA(fit_auto$fit)) {
-      expect_true(all(fit_auto$fit - fit_manual$fit < 1e-14))
-      expect_true(all(fit_auto$se.fit - fit_manual$se < 1e-14)) 
+      # Check the calculation comparing the non-centered versions including the
+      # SEs
+      expect_lt(max(abs(fit_auto$fit - fit_manual$fit)), 1e-14)
+      expect_lt(max(abs(fit_auto$se.fit - fit_manual$se)), 1e-14)
+
+      # Check the centering using the centered version, fits only
+      expect_lt(max(abs(fit_auto_centered - fit_manual_centered)), 1e-14)
     }
   }
 })
